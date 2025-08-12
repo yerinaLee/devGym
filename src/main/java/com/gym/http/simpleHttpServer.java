582c6eeac32f69007298e2d5c6b20d39bcea3d9b
yerinalee <poikl11234@naver.com>
2025-08-12 17:37:37 +0900

    HTTP socket 통신 test

---
+++ src/main/java/com/gym/http/simpleHttpServer.java
@@ -0,0 +1,32 @@
+package com.gym.http;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class simpleHttpServer {
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(8080); // 서버 포트 열기 - 포트 바인드
+        System.out.println("서버 start! http://localhost:8080");
+
+        while (true){
+            Socket socket = serverSocket.accept(); // blocking call. 클라이언트 요청 수락
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            String requestLine = in.readLine(); // 첫 줄만 읽어옴
+            System.out.println("Request : " + requestLine);
+
+            // simple HTTP 응답
+            String body = "<h1>It's me, HTTP!</h1>";
+            out.write("HTTP/1.1 200 OK\r\n");
+            out.write("content-Type: text/html\r\n");
+            out.write("Content-Length: " + body.getBytes().length + "\r\n");
+            out.write("\r\n");
+            out.write(body);
+
+            out.flush();
+            socket.close();
+        }
+    }
+}
